ed8e8ddb45b2ee0c639d6da883a494d0b85c20c4
mvdhoek <mvdhoek75@gmail.com>
2018-11-06 11:56:49 +0100

    Made changes to the style sheet and app.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { ReactiveBase, DataSearch } from '@appbaseio/reactivesearch';
+
+import logo from './DSB_logo.svg';
 import './App.css';
+import theme from './theme';
+import Results from './components/Results';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <section className="container">
+        <ReactiveBase
+          app="ovvdocumenten"
+	  url="http://172.16.1.96:9200"
+          theme={theme}
+        >
+          <nav className="navbar">
+            <div className="title">Safety <font color="4a97b5">Re</font>commendations <font color="4a97b5">Se</font>arch <font color="4a97b5">T</font>ool - <font color="4a97b5">ReSeT</font></div>
+          </nav>
+
+	// Adding the DataSearch here
+	  <div className="flex row-reverse app-container">
+	    <div className="results-container">
+	      <DataSearch
+	          componentId="searchbar"
+		  filterLabel="Search"
+		  dataField={['attachment.author', 'attachment.title', 'attachment.keywords', 'filename']}
+		  placeholder="Search recommendations ..."
+		  autosuggest={false}
+		  iconPosition="left"
+		  URLParams
+		  className="data-search-container results-container"
+		  innerClass={{
+		      input: 'search-input',
+		  }}
+	      />
+	      <Results />
+	    </div>
+	  </div>
+
+        </ReactiveBase>
+      </section>
     );
   }
 }
-
 export default App;
